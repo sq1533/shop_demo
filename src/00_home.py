201b8f71674f8f1c7fa1ae3642f3f3bc13ec3007
sq1533 <618356@naver.com>
2025-03-24 22:29:34 +0900

    feat : home UI 1차 완성

--- src/00_home.py
+++ src/00_home.py
@@ -1,9 +1,37 @@
 import flet
 import shop_demo.src.widget as WG
+import json
+
+with open(file="../storage/data/products.json", mode="r", encoding="utf-8") as f:
+    product = json.load(fp=f)
+
+productCount = list(product["item"]).__len__()
 
 async def main(page: flet.Page):
+    # 상품 on_hover 이벤트
+    def hoverItem(e):
+        e.control.image.opacity = 0.7 if e.data == "true" else 1
+        e.control.update()
+    # like 클릭 이벤트
+    def favoriteIcon(e):
+        icon_widget = e.control.content
+        if icon_widget.name == flet.Icons.FAVORITE_BORDER:
+            icon_widget.name = flet.Icons.FAVORITE
+        else:
+            icon_widget.name = flet.Icons.FAVORITE_BORDER
+        page.update()
+
     page.window.max_width = 600
     page.window.resizable = True
+    page.scroll = flet.ScrollMode.HIDDEN
+
+    itemCards = []
+    for i in range(1, productCount+1):
+        item = WG.itemBox(image=product["item"][f"item_{i}"], hoverE=hoverItem, clickE=favoriteIcon)
+        itemCards.append(item)
+
+    page.appbar = WG.APPBar(image=product["logo"])
+    page.bottom_appbar = WG.bottomNavBar()
 
     mainPage = flet.Container(
         alignment=flet.alignment.top_center,
@@ -13,7 +41,15 @@ async def main(page: flet.Page):
             color=flet.Colors.BLACK45
         ),
         content = flet.Column(
-            controls = [],
+            alignment = flet.MainAxisAlignment.START,
+            horizontal_alignment= flet.CrossAxisAlignment.CENTER,
+            controls = [
+                WG.vannerBox(image = product["recommend"]["item_1"], width=(page.window.width*0.8), height=(page.window.width*0.8)),
+                flet.Row(
+                    alignment = flet.MainAxisAlignment.CENTER,
+                    controls = itemCards,
+                    )
+                ],
         )
     )
 
--- src/widget.py
+++ src/widget.py
@@ -1,37 +1,128 @@
 import flet
 
-def vannerBox(image:str, width, height) -> flet.Image:
+def APPBar(image:dict) -> flet.AppBar:
+    return flet.AppBar(
+            leading = flet.Container(
+                    alignment = flet.alignment.center,
+                    url = image["url"],
+                    url_target = flet.UrlTarget.TOP,
+                    image = flet.DecorationImage(
+                        src = image["src"],
+                        fit = flet.ImageFit.FILL,
+                    ),
+                ),
+            leading_width = 100,
+            center_title=False,
+            bgcolor = flet.Colors.BROWN_400,
+            actions = [
+                flet.Container(
+                    url = '/login',
+                    url_target = flet.UrlTarget.TOP,
+                    content = flet.Text(
+                        "Login / 회원가입"
+                        ),
+                    ),
+                flet.PopupMenuButton(
+                    elevation = 100,
+                    icon = flet.Icons.MENU,
+                    icon_color = flet.Colors.WHITE,
+                    items=[
+                        flet.PopupMenuItem(
+                            text = "Glesses",
+                            checked = False,
+                            on_click = None,
+                            ),
+                        flet.PopupMenuItem(
+                            text = "Sun Glesses",
+                            checked = False,
+                            on_click = None,
+                        ),
+                        flet.PopupMenuItem(
+                            text = "Checked item",
+                            checked = False,
+                            on_click = None,
+                        ),
+                    ]
+                ),
+            ],
+        )
+
+def bottomNavBar() -> flet.BottomAppBar:
+    return flet.BottomAppBar(
+        bgcolor = flet.Colors.DEEP_ORANGE_700,
+        shape = flet.NotchShape.CIRCULAR,
+        content = flet.Row(
+            alignment = flet.MainAxisAlignment.SPACE_EVENLY,
+            controls=[
+                flet.IconButton(icon=flet.Icons.MENU, icon_color=flet.Colors.WHITE),
+                flet.IconButton(icon=flet.Icons.SEARCH, icon_color=flet.Colors.WHITE),
+                flet.IconButton(icon=flet.Icons.FAVORITE, icon_color=flet.Colors.WHITE),
+            ]
+        ),
+    )
+
+def vannerBox(image:dict, width, height) -> flet.Image:
     return flet.Image(
-        src = image,
+        src = image["src"],
         fit = flet.ImageFit.FILL,
         width = width,
         height = height,
     )
 
-def itemBox(image:str) -> flet.Card:
+def itemBox(image:dict, hoverE, clickE) -> flet.Card:
     return flet.Card(
-        content = flet.Container(
-            content = flet.Column(
-                controls = [
-                    flet.Image(
-                        src = image,
-                        width = 300,
-                        height = 200
-                    ),
-                    flet.ListTile(
-                        leading=flet.Icon(flet.Icons.ALBUM),
-                        title=flet.Text("The Enchanted Nightingale"),
-                        subtitle=flet.Text(
-                            "Music by Julie Gable. Lyrics by Sidney Stein."
+        color = flet.Colors.BROWN_100,
+        clip_behavior = flet.ClipBehavior.HARD_EDGE,
+        elevation = 3,
+        width = 180,
+        height = 300,
+        content = flet.Column(
+            alignment = flet.MainAxisAlignment.START,
+            controls = [
+                flet.Container(
+                    alignment = flet.alignment.center,
+                    height = 180,
+                    url = image["url"],
+                    url_target = flet.UrlTarget.PARENT,
+                    on_hover = hoverE,
+                    image = flet.DecorationImage(
+                        src = image["src"],
+                        fit = flet.ImageFit.FILL,
+                        opacity = 1,
+                    ),),
+                flet.Container(
+                    alignment = flet.alignment.center_left,
+                    margin = flet.margin.only(10, 0, 0, 0),
+                    content = flet.Column(
+                        alignment = flet.MainAxisAlignment.START,
+                        controls = [
+                            flet.Text(
+                                image["name"],
+                                color = flet.Colors.GREY_700,
+                                weight = flet.FontWeight.W_500,
+                                ),
+                            flet.Text(
+                                f"{image["price"]:,}",
+                                color = flet.Colors.GREY_700,
+                                weight = flet.FontWeight.W_900,
+                                ),
+                            ],),
                         ),
-                    ),
-                    flet.Row(
-                        [flet.TextButton("Buy tickets"), flet.TextButton("Listen")],
-                        alignment=flet.MainAxisAlignment.END,
-                    ),
-                ]
-            ),
-            width = 400,
-            height = 800,
+                flet.Container(
+                    alignment = flet.alignment.center_left,
+                    margin = flet.margin.only(0, 0, 10, 0),
+                    content = flet.Row(
+                        alignment = flet.MainAxisAlignment.END,
+                        controls = [
+                            flet.Container(
+                                on_click = clickE,
+                                content = flet.Icon(
+                                    name = flet.Icons.FAVORITE_BORDER,
+                                    color = flet.Colors.RED_700,
+                                    size = 30,
+                                ),
+                            ),
+                        ],)
+                )   ,
+                ],),
         )
-    )
